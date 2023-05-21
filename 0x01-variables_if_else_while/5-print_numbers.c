564d6a77a21733bc7ff3c14808e4e749cf796a1a
hassanfardosa <hassanfardosa94@gmail.com>
2023-05-21 00:10:04 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main function
+ * :description :getting numbers of base 10
+ * return : always 0
+ */
+
+int main(void)
+{
+
+
+
+
+int x=0;
+while(x<=9)
+{
+
+printf("%d\n", x);
+
+x++;
+}
+
+
+return 0;
+}
