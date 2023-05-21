d5f0f2fdf495d30de0b2add9f9bfb7d67768acfa
hassanfardosa <hassanfardosa94@gmail.com>
2023-05-20 22:41:01 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * description':print alphabets using putchar'
+ * return : always o
+ */
+
+int main(void)
+
+{
+
+
+	int x=97;
+	while(x <=122)
+	{
+		putchar(x);
+		x++;
+	}
+		putchar('\n');
+
+		return 0;
+
+
+
+
+
+
+
+
+}
