dd358a58787653d04b1aa6b5f377bcc6af13ec82
hassanfardosa <hassanfardosa94@gmail.com>
2023-05-20 23:43:07 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+
+
+
+int x=97;
+while(x<=122)
+{
+if (x==101 || x==113)
+{
+
+x++;
+ continue;
+}
+
+putchar (x);
+x++;
+
+}
+
+
+putchar('\n');
+
+return 0;
+}
+
